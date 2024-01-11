58ede44723a669bfbd5db856f74d0887183eafc0
Arthur Ish <arthurishdev@gmail.com>
2024-01-11 03:55:34 -0800

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function mondayWork(activity = `go to the office`){
+    return `This Monday, I will ${activity}.`;
+}
+function saturdayFun(activity = `roller-skate`){
+    return `This Saturday, I want to ${activity}!`
+}
+function wrapAdjective(flair = `*`){
+    const innerFunction = function (adjective = `special`){
+        return `You are ${flair}${adjective}${flair}!`
+    };
+    return innerFunction;
+}
